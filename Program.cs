66967dfc4fc4cbfaf4a3dddcfb9795d114dcb0ce
Paulo Kenji <paulomuneischi@gmail.com>
2022-05-11 11:10:02 -0300

    adicionado o README e as funções new e list para laboratorios

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ command.CommandText = @"
         ram varchar(100) not null,
         processor varchar(100) not null
     );
+
+    CREATE TABLE IF NOT EXISTS Laboratories(
+        id int not null primary key,
+        number int not null,
+        name varchar(100) not null,
+        block var not null
+    )
     
 ";
 command.ExecuteNonQuery();
@@ -73,5 +80,56 @@ if(modelName == "Computer")
     if(modelAction == "Delete")
     {
         Console.WriteLine("Computer Delete");
+    }
+}
+
+if(modelName == "Laboratory"){
+
+    if (modelAction == "List")
+    {
+        connection = new SqliteConnection("Data Source = database.db");
+        connection.Open();
+
+        command = connection.CreateCommand();
+
+        command.CommandText = "SELECT * FROM Laboratories";
+
+        var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine(
+                "{0},{1},{2},{3}",reader.GetInt32(0),reader.GetInt32(1),reader.GetString(2),reader.GetChar(3)
+            );
+        }
+
+        reader.Close();
+        connection.Close();
+    }
+
+    if (modelAction == "New")
+    {
+        var id = Convert.ToInt32(args[2]);
+        var number = Convert.ToInt32(args[3]);
+        var name = args[4];
+        var block = args[5];
+
+        connection = new SqliteConnection("Data Source = database.db");
+
+        connection.Open();
+
+        command = connection.CreateCommand();
+
+        command.CommandText = @"
+            INSERT INTO Laboratories VALUES($id, $number, $name, $block);
+        ";
+        command.Parameters.AddWithValue("$id",id);
+        command.Parameters.AddWithValue("$number",number);
+        command.Parameters.AddWithValue("$name",name);
+        command.Parameters.AddWithValue("$block",block);
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
     }
 }
